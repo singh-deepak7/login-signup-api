eeec4fbd217010fe76b8856f24849741b504c6be
singh-deepak7 <singh-deepak@outlook.com>
2022-06-29 12:29:30 -0500

    Initial commit

---
+++ src/main/java/com/login/signup/LoginSignupApiApplication.java
@@ -0,0 +1,13 @@
+package com.login.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSignupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSignupApiApplication.class, args);
+	}
+
+}
